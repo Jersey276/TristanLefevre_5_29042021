631bfb432afea3a7be294248e62ed97dd66b2598
Tristan <tristan-lefevre@hotmail.fr>
2021-05-12 02:30:33 +0200

    add token generator + creation of tokenGenerator for generate a random key for token

---
+++ core/auth/tokenGenerator.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace core\auth;
+class tokenGenerator {
+	/**
+     * @param int $lenght : Permet de générer une chaine de caractères aléatoire de 60 caractères par défaut
+     * @return String
+     */
+    public static function strRandom(int $lenght = 60): String
+    {
+        $alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return substr(str_shuffle(str_repeat($alphabet, $lenght)), 0, $lenght);
+    }
+}
