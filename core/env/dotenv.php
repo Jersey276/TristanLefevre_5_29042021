5ace04e50abf4ff374573c50977ce4aac49a7f9c
Tristan <tristan-lefevre@hotmail.fr>
2021-05-12 17:47:42 +0200

    fix dotenv system = fix superglobal problem on dotenv

--- core/env/dotenv.php
+++ core/env/dotenv.php
@@ -37,7 +37,7 @@ class DotEnv
             $name = trim($name);
             $value = trim($value);
 
-            if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+            if (!array_key_exists($name, filter_input_array(INPUT_SERVER)) && !array_key_exists($name, filter_input_array(INPUT_ENV))) {
                 putenv(sprintf('%s=%s', $name, $value));
                 $_ENV[$name] = $value;
                 $_SERVER[$name] = $value;
