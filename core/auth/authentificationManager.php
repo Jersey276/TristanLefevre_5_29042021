9eb8d6fd38b346ec85513df66b1900dc37a0478b
Tristan <tristan-lefevre@hotmail.fr>
2021-05-18 22:02:06 +0200

    add try limit system + creation of limit system related functions + add getIpAddress for collect user ip

--- core/auth/authentificationManager.php
+++ core/auth/authentificationManager.php
@@ -30,8 +30,9 @@ class AuthentificationManager
 	}
 	
 	/**
-	 * 
-	 * @param 
+	 * get instance of this class (Singleton)
+	 * @param databaseManager database manager for all database related operations
+	 * @return authentificationManager instance of this class
 	 */
 	public static function getInstance($database = null)
 	{
@@ -43,33 +44,69 @@ class AuthentificationManager
 	}
 
 	/**
+	 * check if user have remaining try by his IP Adress
+	 * @return bool result
+	 */
+	private function checkTry()
+	{
+		$request = new RequestManager();
+		$try = $this->database->prepare((new SelectQuery())->select('*')->from('loginlog')->where('TIMEDIFF( NOW(), timestamp) < "00:01:00"')->where("ipAddress ='" . $request->getIpAddr() . "'")->toString(),"select","loginLog");
+		if (count($try) < 3)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * report last try
+	 * @return bool validation of process
+	 */
+	private function reportTry()
+	{
+		$request = new RequestManager();
+		return $this->database->prepare((new InsertQuery())
+			->insertInto('loginlog')
+			->key('ipAddress')
+			->value($request->getIpAddr())
+			->toString(),'insert');
+	}
+
+	/**
 	 * get user and fill session var with 
 	 * @param string login
 	 * @param string password
+	 * @return array result with message in case of connection fail
 	 */
 	public function login($login, $password) 
 	{
-		$query = new SelectQuery('select');
-		$statement = $query
-			->select('user.pseudo','user.email','user.password','role.nameRole as role','user.isEmailChecked')
-			->from('user')
-			->leftjoin('role','role.idRole = user.idRole')
-			->where("pseudo = '".$login."'")
-			->toString();
-		$account = $this->database->prepare($statement,"select", "\app\model\User", true);
-		if ($account != false && password_verify($password, $account['password']))
+		if ($this->checkTry())
 		{
-			if($account['isEmailChecked'] == true) 
+			$query = new SelectQuery('select');
+			$statement = $query
+				->select('user.pseudo','user.email','user.password','role.nameRole as role','user.isEmailChecked')
+				->from('user')
+				->leftjoin('role','role.idRole = user.idRole')
+				->where("pseudo = '".$login."'")
+				->toString();
+			$account = $this->database->prepare($statement,"select", "\app\model\User", true);
+			if ($account != false && password_verify($password, $account['password']))
 			{
-				$session = new requestManager();
-				$session->session('pseudo', $account['pseudo']);
-				$session->session('email', $account['email']);
-				$session->session('role', $account['role']);
-				return ['isConnected' => true];
+				if($account['isEmailChecked'] == true) 
+				{
+					$this->reportTry();
+					$request = new RequestManager();
+					$request->session('pseudo', $account['pseudo']);
+					$request->session('email', $account['email']);
+					$request->session('role', $account['role']);
+					return ['isConnected' => true];
+				}
+				return ["isConnected" => false, "message" => "l'adresse mail n'a pas été vérifié, consulter vos mails"];
 			}
-			return ['isConnected' => false, 'message' => "l'adresse mail n'a pas été vérifié, consulter vos mails"];
+			$this->reportTry();
+			return ["isConnected" => false, "message" => "identifiant / mot de passe incorrect, veuiller ressayer"];
 		}
-		return [ 'isConnected' => false, 'message' => 'identifiant / mot de passe incorrect, veuiller ressayer'];
+		return ["isConnected" => false, "message" => "vous avez fais trop d' essai, veuiller attendre 1 minute avant de recommencer"];
     }
 
 	/**
