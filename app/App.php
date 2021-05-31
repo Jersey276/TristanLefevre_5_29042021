92897c4fe6d0e13a9969900bcb3e1570e2836227
Tristan <tristan-lefevre@hotmail.fr>
2021-06-01 00:34:39 +0200

    add setlocal and comment on App + modify composer + add comment and psr structure on app.php + add setlocal and .env loading on load function = modify composer for remove unused package

--- app/App.php
+++ app/App.php
@@ -3,22 +3,37 @@
 
 namespace app;
 
-use core\database\databaseManager;
+use core\database\DatabaseManager;
+use core\env\DotEnv;
 
-class App {
+class App
+{
+    /**
+     * @var DatabaseManagerinstance of database manager
+     */
     private static $db;
-    static function load()
+
+    /**
+     * load all base function, like autoloader, local time and env var
+     */
+    public static function load()
     {
-        if(session_id() == '')
-        {
+        if (session_id() == '') {
             session_start();
         }
         require '../vendor/autoload.php';
+        (new DotEnv(dirname(__DIR__, 1) . '/.env'))->load();
+        setlocale(LC_ALL, 'French_France.1252', 'fr_FR', 'fr_FR@euro', 'fr_FR.utf8');
+        date_default_timezone_set('Europe/Paris');
     }
-    static function getDB()
+
+    /**
+     * get databaseManager instance
+     * @return DatabaseManager instance of database manager
+     */
+    public static function getDB() : DatabaseManager
     {
-        if (empty(self::$db))
-        {
+        if (empty(self::$db)) {
             self::$db = new DatabaseManager();
         }
         return self::$db;
